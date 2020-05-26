6bc7723d19e93b30f5e8b610faf9562f51b438f0
Nellen <nellencr@gmail.com>
2020-05-26 20:09:40 +0200

    Updates

---
+++ precache-manifest.297ec48247827faf5ec9b639381549da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0ca0a316a9382d6c07731b6b82b1f4",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "f92cd9b7e7d78ebf0582",
+    "url": "/Recipe-App/static/css/main.140752de.chunk.css"
+  },
+  {
+    "revision": "4cd2c81119e693c7731d",
+    "url": "/Recipe-App/static/js/2.c8a6e9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.c8a6e9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92cd9b7e7d78ebf0582",
+    "url": "/Recipe-App/static/js/main.b3f84486.chunk.js"
+  },
+  {
+    "revision": "e110fb490e6ab6bdbab9",
+    "url": "/Recipe-App/static/js/runtime-main.c161cdc5.js"
+  }
+]);
